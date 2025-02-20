cada15ad41537883b31cbdf7069e6dab066b1383
Apisapple <38449976+Apisapple@users.noreply.github.com>
2025-02-21 02:09:24 +0900

    :tada: Initialize demo-commerce project with Spring Boot setup and Gradle configuration

---
+++ demo-commerce/src/main/java/mykim/democommerce/DemoCommerceApplication.java
@@ -0,0 +1,13 @@
+package mykim.democommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCommerceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoCommerceApplication.class, args);
+  }
+
+}
---
+++ demo-commerce/src/test/java/mykim/democommerce/DemoCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package mykim.democommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCommerceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
