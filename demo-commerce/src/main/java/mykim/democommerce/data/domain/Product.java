b4ec6b7f5c8a22f0628636866c14000c7f45104a
Apisapple <38449976+Apisapple@users.noreply.github.com>
2025-02-24 04:34:16 +0900

    :package: Add Product and Member entity classes with basic attributes

---
+++ demo-commerce/src/main/java/mykim/democommerce/data/domain/Product.java
@@ -0,0 +1,35 @@
+package mykim.democommerce.data.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "product_type")
+public class Product {
+
+  @Id
+  @Column(name = "product_id")
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  private String name;
+
+  private Integer price;
+
+  private Integer stockQuantity;
+
+  public void addStock(int quantity) {
+    this.stockQuantity += quantity;
+  }
+
+  public void subStock(int quantity) {
+    this.stockQuantity -= quantity;
+  }
+}
